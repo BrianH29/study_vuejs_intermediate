93cf9f3c8ce24c637eb7160aad99bdd49d49138d
BrianH <brianhwang29@gmail.com>
2021-07-22 15:33:37 +0900

    new structure of connecting the input and footer to app

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
         <i class="fas fa-check checkBtn" 
           v-bind:class="{checkBtnCompleted: todoItem.completed}" 
           @click="toggleComplete(todoItem, index)">
@@ -17,11 +17,7 @@
 
 <script>
 export default {
-  data: function(){
-    return {
-      todoItems: []
-    }
-  },
+  props:['propsdata'],
   methods: {
     removeTodo: function(todoItem, index){
       localStorage.removeItem(todoItem); 
@@ -31,15 +27,6 @@ export default {
       todoItem.completed = !todoItem.completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    }
-  },
-  created: function(){
-    if(localStorage.length > 0){
-      for(var i = 0; i<localStorage.length; i++){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
     }
   }
 }
