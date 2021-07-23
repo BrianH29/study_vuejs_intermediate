76a6b5b56198324445ff7652205304d9d4a81e18
BrianH <brianhwang29@gmail.com>
2021-07-23 10:42:39 +0900

    modal and transition

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,32 @@
     <span class="addContainer" @click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">
+          경고!!!
+          <i class="fas fa-times closeModalBtn" @click="showModal = false"></i>
+        </h3>
+
+        <h3 slot="body">HELLO</h3>
+
+        <h6 slot="footer">COPY RIGHT</h6>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
+
 export default {
   data:function(){
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     }
   },
   methods: {
@@ -19,12 +37,17 @@ export default {
       if(this.newTodoItem !== ''){
         this.$emit('addTodoItem', this.newTodoItem)
         this.clearInput(); 
+      }else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function(){
       this.newTodoItem = ''; 
     }
-  }
+  },
+  components: {
+    Modal: Modal
+  },
 }
 </script>
 
@@ -53,5 +76,7 @@ input:focus{
   color: white;
   vertical-align: middle;
 }
-
+.closeModalBtn {
+  color: #42b983;
+}
 </style>
